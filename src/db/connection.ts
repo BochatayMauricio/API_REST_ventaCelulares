044763953f10ef628b8444431fd7438afd9b3a8c
Bochatay Mauricio <bochataymauricio01@gmail.com>
2023-12-11 20:25:41 -0300

    deploy prueba

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,11 +1,8 @@
 import { Sequelize } from 'sequelize';
-import { DB_HOST,DB_NAME,DB_PASSWORD,DB_USER } from '../config';
+import { DB_HOST,DB_NAME,DB_PASSWORD,DB_USER,DB_URL } from '../config';
 
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  dialect: 'mysql'
-})
+const sequelize = new Sequelize(DB_URL, {})
 
 async function testConection() {
   try {
